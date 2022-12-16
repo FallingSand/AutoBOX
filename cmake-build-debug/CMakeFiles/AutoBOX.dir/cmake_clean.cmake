cdeb753cbb439b083ad00df4b6414e9405bab371
FallingSand <2112888131@qq.com>
2022-12-16 12:07:16 +0800

    1

---
+++ cmake-build-debug/CMakeFiles/AutoBOX.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AutoBOX.dir/main.cpp.o"
+  "AutoBOX.pdb"
+  "AutoBOX"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AutoBOX.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,7 @@
+//
+// Created by 21128 on 2022/12/16.
+//
+
+int main() {
+    return 0;
+}
