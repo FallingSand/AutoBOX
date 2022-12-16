712f6b1c056fe0ec635f385e6d9779aa78b04af5
FallingSand <2112888131@qq.com>
2022-12-16 15:11:21 +0800

    1

--- main.cpp
+++ main.cpp
@@ -10,6 +10,10 @@ const char *about_ab_msg = "AutoBOX console is a tool box for easily using compu
                   "AutoBOX 命令行版本 是 一个让电脑使用更轻松的软件\n"
                   "Developer : FallingSand / ";
 
+const char *mode = "exit : exit this program 退出本程序\n"
+                   "aabm (about_auto_box_msg) : about this program 关于本程序\n"
+                   "cls (clear screen) : clear the screen 清空屏幕\n";
+
 string ask_mode_and_return(const string& ask) {
     string answer;
 
@@ -23,7 +27,16 @@ int main() {
     printf("%s", about_ab_msg);
 
     while (true) {
-    string answer = ask_mode_and_return("");
+        string answer = ask_mode_and_return(mode);
+
+        switch (answer) {
+            case "exit" :
+                break;
+            case "aabm" :
+                break;
+            case "cls" :
+                break;
+        }
     }
 
     return 0;
